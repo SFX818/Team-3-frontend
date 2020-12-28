59b6a5bed1f6027817e1bd75b170aee236948d60
Amal Amchtal <amchtala@union.edu>
2020-12-28 14:33:41 -0500

    fixed compile errors

---
+++ src/components/Product.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Product = () => {
+    return (
+        <div className ="container">
+            Insert Code Here
+        </div>
+    )
+}
+export default Product
---
+++ src/components/Purchase.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Purchase = () => {
+    return (
+        <div className ="container">
+            Insert Code Here
+        </div>
+    )
+}
+export default Purchase
