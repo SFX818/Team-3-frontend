7a2cd565fb619de1407d51f6197a35f5311c6f94
nerasan <salima.harun@gmail.com>
2021-01-08 00:57:19 -0600

    adding card

--- src/components/Product.js
+++ src/components/Product.js
@@ -25,17 +25,34 @@ const Product = (props) => {
     }
     
     return (
-        <div className ="container">
-            <img src={props.image}/>
-            <div>Name of Product: {props.name}</div>
-            <div className="description">Description: {props.description}</div>
-            <div>Price: {props.price}</div>
-            <div>User Selling: {props.seller}</div>
-            {/* <form class="delete-form">
-                <button type="button" onClick={handleDelete} className='btn btn-danger'>Delete</button>
-                <button type="button" className="btn btn-link"><Link to={{pathname: '/edit', state: {name: props.name, description: props.description, price: props.price, id: props.id}}}>Edit</Link></button>
-            </form> */}
+
+        <div className="row">
+            <div className="col s12 m6">
+                <div className="card blue-grey darken 1">
+                    <div className="card-content white-text">
+                        <span className="card-title"></span>
+
+                        <p>Card Content</p>
+                    </div>
+                    <div className="card-action">
+                        <a href="#">Edit</a>
+                        <a href="#">Delete</a>
+                    </div>
+                </div>
+            </div>
         </div>
+
+        // <div className ="container">
+        //     <img src={props.image}/>
+        //     <div>Name of Product: {props.name}</div>
+        //     <div className="description">Description: {props.description}</div>
+        //     <div>Price: {props.price}</div>
+        //     <div>User Selling: {props.seller}</div>
+        //     {/* <form class="delete-form">
+        //         <button type="button" onClick={handleDelete} className='btn btn-danger'>Delete</button>
+        //         <button type="button" className="btn btn-link"><Link to={{pathname: '/edit', state: {name: props.name, description: props.description, price: props.price, id: props.id}}}>Edit</Link></button>
+        //     </form> */}
+        // </div>
     )
 }
 
